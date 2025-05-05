8048070e54ccaeb1a08b095e9b3a2d936e79306f
eduardomanhabosco <eduardo.rodrigues@sou.unijui.edu.br>
2025-05-05 20:06:30 -0300

    .

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,13 +4,19 @@ class_name Enemy
 var _loading_dash: bool = false
 var _is_dashing: bool = false
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 var _previous_character_position: Vector2 
 =======
+=======
+>>>>>>> Stashed changes
 <<<<<<< HEAD
 var _previous_character_position: Vector2
 =======
 var _previous_character_position: Vector2 
 >>>>>>> f2662be271868a5faf25b3245df78deacebe8348
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 @export_category("Variables")
