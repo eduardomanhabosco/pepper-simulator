524be79c8e454fd123c9e4e70facd5a29111dfc9
Eduardo Manhabosco <145486963+eduardomanhabosco@users.noreply.github.com>
2025-05-19 23:46:13 -0300

    Spawn de inimigos e Interface

--- management/wave_manager.gd
+++ management/wave_manager.gd
@@ -1,11 +1,107 @@
 extends Node2D
+class_name WaveManager
 
+const _PIZZA_ENEMY: PackedScene = preload("res://enemies/enemy_pizza.tscn")
+const _BURGER_ENEMY: PackedScene = preload("res://enemies/enemy_burger.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var _waves_dict: Dictionary = {
+	1: {
+		"wave_time": 20,
+		"wave_amount": 1,
+		"wave_spawn_cooldown": 4,
+		"spots_amount": [3, 6],
+		"wave_difficulty": "easy"
+	},
+	2: {
+	}
+}
+
+var _current_wave: int = 1
+
+@export_category("Objects")
+@export var _wave_timer: Timer
+@export var _wave_spawner_timer: Timer
+@export var _interface: CanvasLayer = null
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_wave_spawner_timer.start(_waves_dict[_current_wave]["wave_spawn_cooldown"])
+	_wave_timer.start(_waves_dict[_current_wave]["wave_time"])
+	_spawn_enemies()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_wave_timer_timeout() -> void:
+	_current_wave += 1
+	if _current_wave > 10:
+		print("voce venceu")
+		return
+	_wave_timer.start(_waves_dict[_current_wave]["wave_time"])
+
+func _on_wave_spawn_cooldown_timeout() -> void:
+	_spawn_enemies()
+	_wave_spawner_timer.start(_waves_dict[_current_wave]["wave_spawn_cooldown"])
+
+func _spawn_enemies() -> void:
+	var _amount: int = _waves_dict[_current_wave]["wave_amount"]
+	var _spots: Array = []
+
+	for _children in get_children():
+		if not _children is Timer:
+			_spots.append(_children)
+			
+	var _spawn_spot: Array = []
+	for _i in _amount:
+		var _index: int = randi() % _spots.size()
+		var _selected_spot: Node2D = _spots[_index]
+		_spawn_spot.append(_selected_spot)
+		_spots.remove_at(_index)
+		
+	for _spot in _spawn_spot:
+		var _childrens: Array = []
+		var _selected_childrens: Array = []
+		for _children in _spot.get_children():
+			_childrens.append(_children)
+			
+		var _amount_list: Array = _waves_dict[_current_wave]["spots_amount"]
+		var _selected_amount: int = randi_range(_amount_list[0], _amount_list[1])
+		for _i in _selected_amount:
+			var _index: int = randi() % _childrens.size()
+			var _selected_spot: Node2D = _childrens[_index]
+			_selected_childrens.append(_selected_spot)
+			_childrens.remove_at(_index)
+		for _spawner in _selected_childrens:
+			_spawn_enemy(_spawner)
+
+func _spawn_enemy(_spawner: Node2D) -> void:
+	var _enemy: Enemy = null
+	var _randf: float = randf()
+	var _dificulty: String = _waves_dict[_current_wave]["wave_difficulty"]
+	match _dificulty:
+		"easy":
+			_enemy = _PIZZA_ENEMY.instantiate()
+		"easy_to_medium":
+			if _randf <= 0.85:
+				_enemy = _PIZZA_ENEMY.instantiate()
+			else: 
+				_enemy = _BURGER_ENEMY.instantiate()
+		"medium":
+			if _randf <= 0.6:
+				_enemy = _PIZZA_ENEMY.instantiate()
+			else: 
+				_enemy = _BURGER_ENEMY.instantiate()
+		"medium_to_hard":
+			if _randf <= 0.25:
+				_enemy = _PIZZA_ENEMY.instantiate()
+			else: 
+				_enemy = _BURGER_ENEMY.instantiate()
+		"hard":
+			if _randf <= 0.1:
+				_enemy = _PIZZA_ENEMY.instantiate()
+			else: 
+				_enemy = _BURGER_ENEMY.instantiate()
+
+	_enemy.global_position = _spawner.global_position
+	get_parent().call_deferred("add_child", _enemy)
+
+
+func _on_current_time_timer_timeout() -> void:
+	_interface.update_wave_and_time_label(_current_wave, _wave_timer.time_left)
